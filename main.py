0d212efd5a1f722750f1c988d43b7cb62f530a09
Dudleiskac <dudleiskac@usf.edu>
2026-08-26 10:47:22 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+
+# Dudleiska Cruz # Dudleiskac@usf.edu #
+# ----------------------------------- #
+# Main.py: prints *Hello, World! and  #
+# examines behavior of data types.    #
+#######################################
+
+# *we want this script to PRINT the string Hello, World!
+
+print("Hello, World!")
+
+a = "hello world!"
+print(a)
