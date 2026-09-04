c3366d2c99b85b231bd5f70e9169d64b6822e040
Dudleiskac <dudleiskac@usf.edu>
2026-09-04 16:23:51 -0400

    Add files via upload

---
+++ heart rate lab hw.py
@@ -0,0 +1,40 @@
+def patient_information():
+    name=input("enter your name: ")
+    heart_rate=input("enter your heart rate: ")
+    return name,heart_rate
+def patient_information2():
+    name=input("enter your name: ")
+    heart_rate=input("enter your heart rate: ")
+    return name,heart_rate
+def patient_information3():
+    name=input("enter your name: ")
+    heart_rate=input("enter your heart rate: ")
+    return name,heart_rate
+def patient_information4():
+    name=input("enter your name: ")
+    heart_rate=input("enter your heart rate: ")
+    return name,heart_rate
+def patient_information5():
+    name=input("enter your name: ")
+    heart_rate=input("enter your heart rate: ")
+    return name,heart_rate
+def patient_information6():
+    name=input("enter your name: ")
+    heart_rate=input("enter your heart rate: ")
+    return name,heart_rate
+def patient_information7():
+    name=input("enter your name: ")
+    heart_rate=input("enter your heart rate: ")
+    return name,heart_rate
+def patient_information8():
+    name=input("enter your name: ")
+    heart_rate=input("enter your heart rate: ")
+    return name,heart_rate
+print(patient_information())
+print(patient_information2())
+print(patient_information3())
+print(patient_information4())
+print(patient_information5())
+print(patient_information6())
+print(patient_information7())
+print(patient_information8())
